59f2f4d13fdac1bbedd349edcbe02bd398214ddc
root <malikovance@gmail.com>
2024-09-01 23:34:42 +0000

    whitespace

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -2,5 +2,5 @@
 # Author - Malik Vance
 
 """Print numbers 0 to 98 in deimal and hexadecimal."""
-for number in range(0,99):
+for number in range(0, 99):
     print("{} = {}".format(number, hex(number)))
