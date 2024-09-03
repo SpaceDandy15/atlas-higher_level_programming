d3981b5337719cf2b97ab44180ac997966e9ac37
root <malikovance@gmail.com>
2024-09-03 01:33:16 +0000

    blank line whitepace line 6 and 9

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,8 +3,8 @@ def add_tuple(tuple_a, tuple_b):
 
     tuple_a = tuple_a + (0, 0)
     tuple_b = tuple_b + (0, 0)
-    
+
     # Add corresponding elements of both tuples
     new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    
+
     return new_tuple
