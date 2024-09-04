2abbcf7858dc9a3c4cd394111cd031ff3e4ca3ac
root <malikovance@gmail.com>
2024-09-04 03:28:27 +0000

    function that replaces or adds key/value in a dictionary'

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    a function that replaces / adds key
+    value in a dictionary
+    """
+    a_dicitonary[key] = value
+    return a_dictionary
