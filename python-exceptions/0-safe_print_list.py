e57e0489687f2723a88e68251cbd69b948662dc2
root <malikovance@gmail.com>
2024-09-04 04:49:11 +0000

    fixed indention

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,8 @@
-#!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    """print x elements of a list.
+    """Print x elements of a list.
     
     args:
-    my_list (list): the list to print elements form.
+    my_list (list): the list to print elements from.
     x (int): the number of elements of my_list to print.
 
     Returns:
@@ -17,5 +15,5 @@ def safe_print_list(my_list=[], x=0):
             ret += 1
         except IndexError:
             break
-     print("")
-    return (ret)
+    print("")
+    return ret
