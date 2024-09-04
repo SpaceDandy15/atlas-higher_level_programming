9e94d2c0cc58cf3896c241b0e69ef29056fbb1df
root <malikovance@gmail.com>
2024-09-04 04:14:05 +0000

    fixed )))

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -6,7 +6,7 @@ def best_score(a_dictionary):
     function that returns a key with the biggest integer value.
     """
     if a_dictionary:
-        my_list = list(a_dictionary.keys()))
+        my_list = list(a_dictionary.keys())
         score = 0
         leader = ""
         for i in my_list:
