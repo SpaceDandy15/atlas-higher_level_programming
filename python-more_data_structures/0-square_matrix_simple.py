d0cf2f861353588809435957fe3dfd3bfd491ec3
root <malikovance@gmail.com>
2024-09-04 01:43:38 +0000

    moved return statement

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -9,4 +9,4 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:
         result = list(map(lambda x: x**2, row))
         new_matrix.append(result)
-        return new_matrix
+    return new_matrix
