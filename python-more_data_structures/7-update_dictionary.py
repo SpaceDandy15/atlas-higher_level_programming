063b89844c12ad5024f2e7b04e770db82515cdc2
root <malikovance@gmail.com>
2024-09-04 03:30:26 +0000

    dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -6,5 +6,5 @@ def update_dictionary(a_dictionary, key, value):
     a function that replaces / adds key
     value in a dictionary
     """
-    a_dicitonary[key] = value
+    a_dictionary[key] = value
     return a_dictionary
