3762b9ef50aec9837f284736fc82b66af7310238
root <malikovance@gmail.com>
2024-09-04 01:36:49 +0000

    changed matix to matrix and used for row in matrix which iterates over each row of the input matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,12 +2,11 @@
 
 def square_matrix_simple(matrix=[]):
     """
-    wordA function that computes the square
-    value of all integers of a matrix.
+
     """
 
-    new matix = []
-    for col in matrix:
-        result = list(map(lambda x: x**2, col))
+    new_matrix = []
+    for row in matrix:
+        result = list(map(lambda x: x**2, row))
         new_matrix.append(result)
         return new_matrix
