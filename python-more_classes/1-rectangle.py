f2e6ad2b75bea67dcdb95b82fc37b3bec9b838c9
root <malikovance@gmail.com>
2024-09-11 03:48:43 +0000

    fixed 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Defines a Rectangle class."""
+
 
 class Rectangle:
     """Represent a rectangle."""
@@ -21,9 +23,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")  # Fixed typo here
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")  # Fixed error message here
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -34,8 +36,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")  # Fixed typo here
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")  # Fixed error message here
+            raise ValueError("height must be >= 0")
         self.__height = value
 
