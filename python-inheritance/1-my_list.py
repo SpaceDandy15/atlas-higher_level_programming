8b565f12442492c3888db9416570aa0d61c59071
root <malikovance@gmail.com>
2024-09-15 15:36:39 +0000

    alignment issue

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,15 +1,9 @@
-#!/usr/bin/python3
-"""
-contain the Mylist class
-"""
-
-
 class MyList(list):
     """a subclass of list"""
     def __init__(self_):
         """initializes the object"""
         super().__init__()
-    
+
     def print_sorted(self):
         """prints the sorted list"""
         print(sorted(self))
