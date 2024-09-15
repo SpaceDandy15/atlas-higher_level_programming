bab6731d6f455e039b76f1a162af061a9d379b0b
root <malikovance@gmail.com>
2024-09-15 15:03:25 +0000

    a class MyList that inherits from list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+contain the Mylist class
+"""
+
+
+class MyList(list):
+    """a subclass of list"""
+    def __init__(self_):
+        """initializes the object"""
+        super().init__()
+    
+    def print_sorted(self):
+        """prints the sorted list"""
+        print(sorted(self))
--- python-inheritance/README.md
+++ python-inheritance/README.md
@@ -1,1 +1,3 @@
 File 0 is a function that returns the list of available attributes and methods of an object.
+
+File 1 is a class MyList that inherits from list
