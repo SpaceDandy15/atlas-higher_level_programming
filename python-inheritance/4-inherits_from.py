3b3f83df80c12448e9f9b4cb91d582f5c7b95eff
SpaceDandy15 <166963376+SpaceDandy15@users.noreply.github.com>
2024-09-16 04:52:12 +0000

    fixed indentation

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Defines an inherited class-checking function."""
 
-
 def inherits_from(obj, a_class):
     """Checks if an object is an inherited instance of a class.
 
@@ -12,5 +11,4 @@ def inherits_from(obj, a_class):
         If obj is an inherited instance of a_class - True.
         Otherwise - False.
     """
-     return isinstance(obj, a_class) and type(obj) is not a_class
-
+    return isinstance(obj, a_class) and type(obj) is not a_class
