385909d0ab77460561f891d7ade7396b1adc549f
SpaceDandy15 <166963376+SpaceDandy15@users.noreply.github.com>
2024-09-17 16:48:57 +0000

    needed a blank line on 19 and new line at end of file

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -16,6 +16,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
         """Get a dictionary reprsentation of the Student."""
         return self.__dict__
