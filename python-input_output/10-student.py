7f472455ddb20d1cc7d3a38b637d354703e6acc8
SpaceDandy15 <166963376+SpaceDandy15@users.noreply.github.com>
2024-09-17 17:05:34 +0000

    fixed line 29 being too long

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -28,6 +28,8 @@ class Student:
         """
         if isinstance(attrs, list) and all(isinstance(ele, str) for ele in attrs):
             return {
-                k: getattr(self, k) for k in attrs if hasattr(self, k)
+                k: getattr(self, k)
+                for k in attrs
+                if hasattr(self, k)
             }
         return self.__dict__
