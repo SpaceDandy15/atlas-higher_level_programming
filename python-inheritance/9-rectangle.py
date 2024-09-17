441c720af8231f4092bc1e0046604389dc9f5c1a
SpaceDandy15 <166963376+SpaceDandy15@users.noreply.github.com>
2024-09-17 04:03:24 +0000

    pep 8 fixes

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """Initialize a new Rectangle.
-        
+
         Args:
             width (int): The width of the new Rectangle.
             height (int): The height of the new Rectangle.
@@ -21,8 +21,7 @@ class Rectangle(BaseGeometry):
     def area(self):
         """Return the area of the rectangle."""
         return self.__width * self.__height
-    
+
     def __str__(self):
         """Return the print() and str() representation of a Rectangle."""
         return f"[{self.__class__.__name__}] {self.__width}/{self.__height}"
-    
