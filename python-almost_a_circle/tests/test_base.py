245c4858f8dd95313fa0bc3de5c59e90791991e9
SpaceDandy15 <166963376+SpaceDandy15@users.noreply.github.com>
2024-09-22 14:43:34 +0000

    moved files around

---
+++ python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,18 @@
+import unittest
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    def test_id_auto_assignment(self):
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+
+    def test_id_manual_assignment(self):
+        b1 = Base(12)
+        self.assertEqual(b1.id, 12)
+
+if __name__ == "__main__":
+    unittest.main()
