82374977d62a43698f1c7321af726787c6215b04
Malik Vance <malikovance@gmail.com>
2024-10-13 16:02:04 -0500

    comments

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,10 @@
+-- This query retrieves the titles of TV shows along with their associated genre IDs
+-- from the hbtn_0d_tvshows database.
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id  -- Ensure 'show_id' is the correct column
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id  -- Joining tv_shows and tv_show_genres on the show ID
+
+-- The ORDER BY clause sorts the results first by the title of the TV shows
+-- in ascending order and then by genre_id in ascending order.
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
