2e9a1df8c257e73acaa771a2696c98b276928698
Malik Vance <malikovance@gmail.com>
2024-10-15 23:23:57 -0500

    pycode

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -7,10 +7,10 @@ in the hbtn_0e_14_usa database.
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
+
 class City(Base):
     """City class that maps to the cities table."""
     __tablename__ = 'cities'
-
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
