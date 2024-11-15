6cdd31e80627489ca7c71fc4bbfc1b836acbafa1
Malik Vance <malikovance@gmail.com>
2024-11-14 21:54:45 -0600

    semistandard fix

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Define a constant variable
-const myVar = "JavaScript is amazing";
+const myVar = 'JavaScript is amazing';
 
 // Print the constant variable to the console
 console.log(myVar);
