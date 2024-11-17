5b15491b25ae329d67c20afc3aa3c6159ba77f41
Malik Vance <malikovance@gmail.com>
2024-11-16 23:00:39 -0600

    function that returns the addition of 2 integers.

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,10 @@
+// 13-add.js
+
+// Define the function that adds two integers
+function add(a, b) {
+    return a + b;
+  }
+  
+  // Export the function so it can be used in other files
+  module.exports.add = add;
+  
