2f3fcfcd27de524fc459c7f0a54c8e638074fc53
Malik Vance <malikovance@gmail.com>
2024-11-16 23:03:32 -0600

    dleted shebang

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,11 +1,9 @@
 // 13-add.js
-#!/usr/bin/node
 
 // Define the function that adds two integers
 function add(a, b) {
-    return a + b;
-  }
-  
-  // Export the function so it can be used in other files
-  module.exports.add = add;
-  
+  return a + b;
+}
+
+// Export the function so it can be used in other files
+module.exports.add = add;
