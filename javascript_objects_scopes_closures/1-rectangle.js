957ea544b149b396e33e8392535f1d9fd17bd849
Malik Vance <malikovance@gmail.com>
2024-11-17 09:13:42 -0600

    class Rectangle that defines a rectangle

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/**
+ * Rectangle - A class that defines a rectangle.
+ * @class
+ */
+class Rectangle {
+    /**
+     * Creates an instance of Rectangle.
+     * @param {number} w - The width of the rectangle.
+     * @param {number} h - The height of the rectangle.
+     */
+    constructor(w, h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  module.exports = Rectangle;
+  
