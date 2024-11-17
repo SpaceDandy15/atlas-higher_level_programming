1f1a7c858562195e715f36448606238f3e80dd99
Malik Vance <malikovance@gmail.com>
2024-11-16 20:04:44 -0600

    script that prints x times “C is fun”

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// Retrieve the first argument passed
+const arg = process.argv[2];
+
+// Parse the argument to an integer
+const x = parseInt(arg);
+//check if x is not a valid number
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
