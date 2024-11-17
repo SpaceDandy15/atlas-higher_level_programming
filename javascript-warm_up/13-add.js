d1f264ecee1fb495ac6c66dcf745fc481983b84c
Malik Vance <malikovance@gmail.com>
2024-11-16 23:05:43 -0600

    squeezed it into one file

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,9 +1,9 @@
-// 13-add.js
+#!/usr/bin/node
 
 // Define the function that adds two integers
 function add(a, b) {
   return a + b;
 }
 
-// Export the function so it can be used in other files
-module.exports.add = add;
+// Call the function and print the result
+console.log(add(3, 5));  // This should print 8
--- javascript-warm_up/13-main.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-// Require the add function from 13-add.js
-const add = require('./13-add').add;
-
-// Call the function and log the result
-console.log(add(3, 5));  // This should print 8
