eacf9324d2ff2b6c03b517bb3421e117b64477a7
Malik Vance <malikovance@gmail.com>
2024-11-24 11:11:46 -0600

    JavaScript script that updates the text color of the <header> element to red (#FF0000):
    You can’t use document.querySelector to select the HTML tag
    You must use the JQuery API
    
    JavaScript script that updates the text color of the <header> element to red (#FF0000) when the user clicks on the tag DIV#red_header:
    
    You can’t use document.querySelector to select the HTML tag
    You must use the JQuery API

---
+++ javascript-web_jquery/1-script.js
@@ -0,0 +1,2 @@
+// Select the <header> element using JQuery and update its text color
+$('header').css('color', '#FF0000');
---
+++ javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+// Use JQuery to attach a click event to the DIV#red_header element
+$('#red_header').click(function () {
+    // Change the text color of the <header> element to red
+    $('header').css('color', '#FF0000');
+  });
+  
